ad578bcdd35cfddcf79768ad1070971659138566
Boris Zaric <boris.zaric@edu.szu.at>
2024-01-05 18:49:59 +0100

    Erst begonnen mit GUI kopf hoch

---
+++ Helpplaner.Client.GUI/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Helpplaner.Client.GUI
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
